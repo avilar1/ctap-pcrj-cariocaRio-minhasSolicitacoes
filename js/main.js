9c0700f499a3265d2a399aefbcc1a6f17e55c1fe
Yago Vial <yagoavila1@gmail.com>
2022-07-21 17:59:10 -0300

    responsividade e inicio da população dos dados

--- js/main.js
+++ js/main.js
@@ -7,28 +7,81 @@
 // flush-collapseOne
 // accordionbody
 
-//usar replace
 
-const linha = document.querySelector('[data-linha]')
 
-const aflush = document.querySelector('#accordionFlushExample')
-const aitem = document.querySelector('#accordionitem')
-const ahone = document.querySelector('#flush-headingOne')
-const abutton = document.querySelector('#accordionbutton')
-const acone = document.querySelector('#flush-collapseOne')
-const abody = document.querySelector('#accordionbody')
+const linha = document.querySelectorAll('[data-linha]')
 
+const aflushD = document.querySelectorAll('[data-acc="aflush"]')
+const aitemD = document.querySelectorAll('[data-acc="aitem"]')
+const ahoneD = document.querySelectorAll('[data-acc="ahone"]')
+const abuttonD = document.querySelectorAll('[data-acc="abutton"]')
+const aconeD = document.querySelectorAll('[data-acc="acone"]')
+const abodyD = document.querySelectorAll('[data-acc="abody"]')
+
+const acardbD = document.querySelectorAll('[data-acc="acardb"]')
+
+
+
+let m = 0;
+linha.forEach((element) => {
+  //$([aitemD])[0][0].dataset.acc='aa' //modelo
+  $(ahoneD)[m].id = 'flush-heading' + m
+  $(abuttonD)[m].dataset.bsTarget = '#flush-collapse' + m
+  $(abuttonD)[m].setAttribute("aria-controls", 'flush-collapse' + m);
+  $(aconeD)[m].id = 'flush-collapse' + m
+  $(aconeD)[m].setAttribute("aria-labelledby", 'flush-heading' + m);
+  aconeD[m].ariaLabelledby = 'flush-heading' + m
+  m++
+})
+
+let controle = 0;
 
 function myFunction(x) {
-    if (x.matches) { // If media query matches
-      //aitem.removeChild(ahone)
+  if (x.matches) { // If media query matches
 
-      
-    } else {
-      aitem.appendChild(ahone)
+    if (controle == 0) {
+
+      let n = 0
+      linha.forEach((elemento) => {
+        aflushD[n].parentElement.replaceChild(acardbD[n], aflushD[n])
+
+        n++
+      })
+
+      controle = 1;
     }
+
+  } else {
+    if (controle == 1) {
+      let n = 0
+      linha.forEach((element) => {
+
+        element.childNodes[n].parentNode.appendChild(aflushD[n])
+        aflushD[n].appendChild(aitemD[n])
+        aitemD[n].appendChild(ahoneD[n])
+        aitemD[n].appendChild(abuttonD[n])
+        aflushD[n].appendChild(aconeD[n])
+        aconeD[n].appendChild(abodyD[n])
+        abodyD[n].appendChild(acardbD[n])
+        n++
+      })
+      controle = 0;
+    }
+
   }
-  
-  var x = window.matchMedia("(min-width: 780px)")
-  myFunction(x) // Call listener function at run time
-  x.addListener(myFunction) // Attach listener function on state changes
+}
+
+var x = window.matchMedia("(min-width: 780px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
+
+
+
+  //funcionava com 1 item apenas:
+          // element.childNodes[0].parentNode.appendChild(aflush)
+          // aflush.appendChild(aitem)
+          // aitem.appendChild(ahone)
+          // aitem.appendChild(abutton)
+          // aflush.appendChild(acone)
+          // acone.appendChild(abody)
+          // abody.appendChild(cardb)
---
+++ js/pop.js
@@ -0,0 +1,81 @@
+const abuttonPop = document.querySelectorAll('[data-acc="abutton"]')
+
+let control = 0
+
+const pop = {
+    "rowum": {
+        "nso": "CID-REQ-2022/1211",
+        "dso": "17/12/2022",
+        "srv": "Instalação de caçamba",
+        "org": "COMLURB",
+        "uta": "30/12/2022",
+        "sit": "Em andamento"
+    },
+
+    "rowdois": {
+        "nso": "CID-REQ-2022/2874",
+        "dso": "22/5/2022",
+        "srv": "Remoção de entulho e bens inservíveis",
+        "org": "COMLURB",
+        "uta": "24/6/2022",
+        "sit": "Indeferido"
+    },
+    "rowtres":{
+        "nso": "CID-REQ-2022/2452",
+        "dso": "20/5/2022",
+        "srv": "Denunciar obra irregular",
+        "org": "SECONSERVA",
+        "uta": "22/6/2022",
+        "sit": "CUMPRIR EXIGÊNCIA"
+    },
+    "rowquatro":{
+        "nso": "CID-REQ-2022/2014",
+        "dso": "07/5/2022",
+        "srv": "Autorização para colocação de mesas e cadeiras em área pública",
+        "org": "PGM",
+        "uta": "22/5/2022",
+        "sit": "Em andamento"
+    },
+    "rowcinco":{
+        "nso": "CID-REQ-0000/0000",
+        "dso": "14/5/2022",
+        "srv": "Deferido",
+        "org": "COMLURB",
+        "uta": "20/5/2022",
+        "sit": "Indeferido"
+    }
+}
+
+
+tamanho = Object.keys(pop).length;
+
+for (var prop in pop) {   
+    // console.log(pop[prop].nso);
+    let item = document.createElement('div');
+    item.textContent = pop[prop].nso
+    console.log(item)
+}
+
+let item = document.createElement('div');
+
+function myFunction(x) {
+
+    if (x.matches) { // If media query matches
+  
+      if (control == 0) {
+        abuttonPop[1].textContent = 'aa'
+        control = 1;
+      }
+  
+    } else {
+      if (control == 1) {
+
+        control = 0;
+      }
+      
+    }
+  }
+  
+  var x = window.matchMedia("(min-width: 780px)")
+  myFunction(x) // Call listener function at run time
+  x.addListener(myFunction) // Attach listener function on state changes
